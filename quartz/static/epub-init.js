d5c47f6b9ea4f8a76173ddd69a1fd1e42ea6d193
likemie <a690089521@gmail.com>
2026-05-09 01:09:13 +0800

    Quartz sync: May 9, 2026, 1:09 AM

--- quartz/static/epub-init.js
+++ quartz/static/epub-init.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
+window.addEventListener("load", function() {
   var viewer = document.getElementById("epub-viewer");
-  if (viewer) {
+  if (viewer && typeof ePub !== "undefined") {
     var epubPath = viewer.getAttribute("data-epub");
     if (epubPath) loadEpub("epub-viewer", epubPath);
   }
