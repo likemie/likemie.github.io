fa8dd68296b383046be1a0fdb88059dc13b74a7b
likemie <a690089521@gmail.com>
2026-06-10 02:34:22 +0800

    Revert "Use base Mermaid theme for readable diagrams"
    This reverts commit 8936e06efc285cf2dda5262ca6981456c2429f4b.

--- quartz/components/scripts/mermaid.inline.ts
+++ quartz/components/scripts/mermaid.inline.ts
@@ -219,22 +219,19 @@ document.addEventListener("nav", async () => {
       {} as Record<(typeof cssVars)[number], string>,
     )
 
+    const darkMode = document.documentElement.getAttribute("saved-theme") === "dark"
     mermaid.initialize({
       startOnLoad: false,
       securityLevel: "loose",
-      theme: "base",
+      theme: darkMode ? "dark" : "base",
       themeVariables: {
         fontFamily: computedStyleMap["--codeFont"],
         primaryColor: computedStyleMap["--light"],
-        primaryTextColor: computedStyleMap["--dark"],
+        primaryTextColor: computedStyleMap["--darkgray"],
         primaryBorderColor: computedStyleMap["--tertiary"],
         lineColor: computedStyleMap["--darkgray"],
         secondaryColor: computedStyleMap["--secondary"],
         tertiaryColor: computedStyleMap["--tertiary"],
-        textColor: computedStyleMap["--dark"],
-        darkTextColor: computedStyleMap["--dark"],
-        nodeTextColor: computedStyleMap["--dark"],
-        mainBkg: computedStyleMap["--light"],
         clusterBkg: computedStyleMap["--light"],
         edgeLabelBackground: computedStyleMap["--highlight"],
       },
