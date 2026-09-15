2b0819f5921e3125d0f3aef91c20b923e8a97b30
likemie <a690089521@gmail.com>
2026-09-15 19:29:16 +0800

    Exclude internal vault files from publishing

--- quartz.ts
+++ quartz.ts
@@ -2,7 +2,8 @@ import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/conf
 import { componentRegistry } from "./quartz/components/registry"
 
 componentRegistry.setOptionOverrides("explorer", {
-  filterFn: (node: { slugSegment?: string }) => node.slugSegment !== "explore",
+  filterFn: (node: { slugSegment?: string }) =>
+    node.slugSegment !== "explore" && node.slugSegment !== "tags",
 })
 
 const config = await loadQuartzConfig()
